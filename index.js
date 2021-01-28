d9b60eedcb579615a41955de3751ddd75ea74d1f
리펙스 <66472285+xoals35@users.noreply.github.com>
2021-01-28 18:28:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -30,4 +30,94 @@ client.on('message', async message =>{
     if(!command) command = client.commands.get(client.aliases.get(cmd));
     if(command) command.run(client, message, args) 
 })
+
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    let prefix = await db.get(`prefix_${message.guild.id}`);
+    if(prefix === null) prefix = prefix;
+
+    let profile = await leveling.Fetch(message.author.id);
+    leveling.AddXp(message.author.id, 1);
+
+    if(profile.xp + 0 > 150){
+        leveling.AddLevel(message.author.id, 1);
+        leveling.SetXp(message.author.id, 1)
+        message.channel.send(`${message.author.username}님 축하합니다 레벨이${profile.level + 1}으(로)올랐습니다. 레벨업 해야하면 150경험치을 얻어야합니다.(150이 경험치 최대 경험치)`)
+    }
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(token)
