63877bc1c1e2f89eef887fcea0d84234d42cab62
리펙스 <66472285+xoals35@users.noreply.github.com>
2021-01-28 18:21:56 +0900

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,1 +1,18 @@
 
+  
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name : '핑',
+    description : 'Returns latency and API ping',
+
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+
+    run : async(client, message, args) => {
+       message.channel.send(`현재 봇의 응답핑:${client.ws.ping}ms`)
+
+    }
+}
